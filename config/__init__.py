d557f338cf0c74a13044e8eb94f23ec73f248cef
chenlin <lin.chen1@yimifudao.com>
2019-05-13 22:02:46 +0800

    增加__init__.py提交

---
+++ config/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+__all__ = ['celery_app', 'RUN_VER', 'APP_CODE', 'SECRET_KEY', 'BK_URL', 'BASE_DIR']
+
+import os
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from blueapps.core.celery import celery_app
+
+# app 基本信息
+
+# SaaS运行版本，如非必要请勿修改
+RUN_VER = 'open'
+# SaaS应用ID
+APP_CODE = 'hello-world'
+# SaaS安全密钥，注意请勿泄露该密钥
+SECRET_KEY = 'ff101e61-0503-4fa0-ba04-535ede965530'
+# 蓝鲸SaaS平台URL, 如 https://paas.blueking.com/
+# BK_URL = None
+BK_URL = 'http://paas.class.o.qcloud.com:80'
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(
+    __file__)))

64121abc431b93c6550d94e20a8576e4d3af1ae9
chenlin <sona201@163.com>
2019-05-16 15:15:34 +0800

    去掉装饰器，增加认证功能

--- config/__init__.py
+++ config/__init__.py
@@ -24,3 +24,4 @@ BK_URL = 'http://paas.class.o.qcloud.com:80'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(
     __file__)))
+
